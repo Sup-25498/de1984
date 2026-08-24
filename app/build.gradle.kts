61f2309f39e19f2a649eff9fb9eea2ebfc260c36
doru <doru.moraru@icloud.com>
2026-08-24 23:43:25 +0100

    feat: increment version to 2.6.4 and add changelog for improvements and fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 34
-        versionName = "2.6.3"
+        versionCode = 35
+        versionName = "2.6.4"
 
         resourceConfigurations += listOf("en", "ro", "pt", "zh", "it", "fr", "ru")
 
