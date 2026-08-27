4c6e9f232cb6801fb27f8446621336bfc4e344e1
doru <doru.moraru@icloud.com>
2026-08-27 14:38:02 +0100

    Update to 2.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 36
-        versionName = "2.6.5"
+        versionCode = 37
+        versionName = "2.6.6"
 
         resourceConfigurations += listOf("en", "ro", "pt", "zh", "it", "fr", "ru")
 
