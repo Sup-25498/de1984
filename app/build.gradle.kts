719f76d5532eae03203ece0636b0f3b4db05c603
doru <doru.moraru@icloud.com>
2026-08-28 21:12:14 +0100

    feat: update version to 2.6.8 and add changelog for improved screen rebuild handling in Firewall and Packages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 38
-        versionName = "2.6.7"
+        versionCode = 39
+        versionName = "2.6.8"
 
         resourceConfigurations += listOf("en", "ro", "pt", "zh", "it", "fr", "ru")
 
