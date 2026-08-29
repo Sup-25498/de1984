3327a8d86b9049ae774ac2a19e67a2a313ee07d3
doru <doru.moraru@icloud.com>
2026-08-29 14:33:39 +0100

    feat: update version to 2.7.1 and add changelog for improved backend notifications and battery efficiency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         minSdk = 26
         targetSdk = 34
 
-        versionCode = 41
-        versionName = "2.7.0"
+        versionCode = 42
+        versionName = "2.7.1"
 
         resourceConfigurations += listOf("en", "ro", "pt", "zh", "it", "fr", "ru")
 
